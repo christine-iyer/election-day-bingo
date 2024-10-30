a20c022826ea93a8f77ba73d2479ff0b5d8f0afb
chrisIyer <hellohotbox420@gmail.com>
2024-10-30 11:09:39 -0400

    a big clock is there.

---
+++ app/components/Clock.js
@@ -0,0 +1,15 @@
+import  "./clock.css"
+function Clock() {
+
+
+
+  return (
+    <div>
+    
+      <button style ={{margin: "120px", height: '120px', width: '120px'}}onClick={alert("Nice")}>+</button>
+<div className="clock"></div>
+    </div>
+  );
+}
+
+export default Clock;
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import USMap from './components/USMap'
 import BingoCard from "./components/BingoCard"
 import HistoricalMap from './components/HistoricalMap'
+import Clock from './components/Clock'
 
 
 export default function Home() {
@@ -93,7 +94,9 @@ export default function Home() {
             {item.name} - Population: {item.population}
           </li>
         ))}
-      </ul></div>
+      </ul>
+      <Clock/>
+      </div>
     
 
 
